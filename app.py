e6056b16db08bbd41dd81c1b2adaecc7092b2f01
HugoLrdd <hugoloridn@gmail.com>
2026-05-23 19:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_to_file
+from flask import Flask, render_template, request, redirect
 from datetime import datetime
 
 app = Flask(__name__)
